680bfc74bb7204a4c62b2c64c58eaabed220d86d
Jonathan Bobrow <jbobrow@gmail.com>
2018-11-11 19:31:35 -0500

    remove setup/loop duplication

--- berry.ino
+++ berry.ino
@@ -79,12 +79,4 @@ void loop() {
     byte face = (faceStartIndex + faceIndex - 1) % FACE_COUNT;
     setFaceColor( face, colors[nextColorIndex] );
   }
-}void setup() {
-  // put your setup code here, to run once:
-
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
 }
